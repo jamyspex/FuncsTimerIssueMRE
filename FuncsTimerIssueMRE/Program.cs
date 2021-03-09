2165b08a7fd04e04a95e4f5d449c7244cf983105
James Macdonald <james.macdonald@city-holdings.co.uk>
2021-03-09 16:05:00 +0000

    Add project files.

---
+++ FuncsTimerIssueMRE/Program.cs
@@ -0,0 +1,33 @@
+﻿using Autofac.Extensions.DependencyInjection;
+using Microsoft.Azure.Functions.Worker.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace FuncsTimerIssueMRE
+{
+    internal static class Program
+    {
+        #region Public Methods
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    builder.AddJsonFile("appsettings.json", false, true);
+                })
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureFunctionsWorker((_, workerApplicationBuilder) =>
+                {
+                    workerApplicationBuilder.UseFunctionExecutionMiddleware();
+                });
+        }
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        #endregion
+    }
+}
